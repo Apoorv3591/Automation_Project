7986858a1ec36c894ac20c6a8a4c2e4a003cf97e
unknown <apoorv.singh@easygov.co.in>
2022-07-16 22:44:42 +0530

    adding automation script task3

--- automation.sh
+++ automation.sh
@@ -36,3 +36,30 @@ aws s3 cp /tmp/"${myname}-"httpd-access-logs"-${timestamp}.tar" s3://"${s3_bucke
 aws s3 cp /tmp/"${myname}-"httpd-error-logs"-${timestamp}.tar" s3://"${s3_bucket}"/
 
 
+echo "maintaining records"
+file_access=/tmp/"${myname}-"httpd-access-logs"-${timestamp}.tar"
+file_error=/tmp/"${myname}-"httpd-error-logs"-${timestamp}.tar"
+filesize_access=$(ls -lh $file_access | awk '{print  $5}')
+filesize_error=$(ls -lh $file_error | awk '{print  $5}')
+FILERECORD=/var/www/html/inventory.html
+if [ -f "$FILERECORD" ]
+then
+    echo "<p>httpd-access-logs&emsp;&emsp;${timestamp}&emsp;&emsp;tar&emsp;&emsp;${filesize_access}" >> /var/www/html/inventory.html
+    echo "<p>httpd-error-logs&ensp;&emsp;&emsp;${timestamp}&emsp;&emsp;tar&emsp;&emsp;${filesize_error}" >> /var/www/html/inventory.html
+else
+    echo "<p>Log Type&emsp;&emsp;&emsp;&emsp;&emsp;Time Created&emsp;&emsp;&emsp;&emsp;Type&emsp;&emsp;Size</p>" >> /var/www/html/inventory.html
+    echo "<p>httpd-access-logs&emsp;&emsp;${timestamp}&emsp;&emsp;tar&emsp;&emsp;${filesize_access}" >> /var/www/html/inventory.html
+    echo "<p> httpd-error-logs&ensp;&emsp;&emsp;${timestamp}&emsp;&emsp;tar&emsp;&emsp;${filesize_error}" >> /var/www/html/inventory.html
+fi
+
+
+
+
+echo "scheduling jobs every 2 hour"
+FILECRON=/etc/cron.d/automation
+if [ -f "$FILECRON" ]
+then
+    echo "job already scheduled"
+else
+    echo "0 */2 * * * /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+fi
